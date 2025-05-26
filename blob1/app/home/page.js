f99befeaa1a1ac46da2fb7769c58c0285f17865e
wasteapp111@gmail.com <Diordi1>
2025-05-26 11:51:01 +0530

    blog-mainContent

--- blob1/app/home/page.js
+++ blob1/app/home/page.js
@@ -1,6 +1,13 @@
+// import Card from "../_components/Card";
+import MainContent from "../_components/MainContent";
+
 function Home1() {
     return (
-        <div>Home</div>
+        <div className="w-[100%] h-[100%] flex items-center justify-center flex-col">
+
+        <MainContent/>
+        {/* <Card/> */}
+        </div>
       );
 }
 
